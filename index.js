2062903b6500e112996ae26da2f6b77d7aa58ac0
StarQueen <157178713+StarQueen342@users.noreply.github.com>
2024-02-19 21:21:51 -0600

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Collection, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { clientId, token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
 
 client.commands = new Collection();
 
